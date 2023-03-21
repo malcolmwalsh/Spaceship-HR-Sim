4144f7f2fcbf71504aa4292e3c70e3604801c630
Mal Walsh <malcolmwalsh@gmail.com>
2023-03-21 16:39:29 +1100

    Roster is animated

--- Assets/Game/Control/GameManager.cs
+++ Assets/Game/Control/GameManager.cs
@@ -1,4 +1,5 @@
 using Assets.Game.Objects.Crewmates;
+using Assets.Game.Objects.Hunts;
 using Assets.Game.Objects.Rosters;
 using System;
 using System.Collections.Generic;
@@ -12,6 +13,8 @@ namespace Assets.Game.Control
         [SerializeField] private Roster roster;
         [SerializeField] private CrewmateFactory factory;
         [SerializeField] private StatController stats;
+        [SerializeField] private Hunt hunt;
+        [SerializeField] private Transform airlock;
 
         private Crewmate potentialCrewmate;
 
@@ -72,6 +75,11 @@ namespace Assets.Game.Control
                 // Is parasite
                 ui.AppendToLog($"{potentialCrewmate} is a parasite! The crew is in mortal danger now.");
 
+                // Change sprite
+                parasite.SwapSprite();
+
+                MoveToAirlock(parasite);
+
                 BeginHunt(parasite);
             }
 
@@ -80,6 +88,17 @@ namespace Assets.Game.Control
 
             // Let player create a new one
             waitingForUser = false;
+        }
+
+        private void MoveToAirlock(ParasiteCrewmate parasite)
+        {
+            // Move to airlock
+            parasite.transform.SetParent(airlock);
+            parasite.transform.localScale = new Vector3(0.5F, 0.5F, 1);
+
+            // Destination position
+            Vector3 destination = airlock.position + new Vector3(stats.NumHunts, 0, 0);
+            parasite.SetDestination(destination, 1);
         }
 
         private void RefuseCrewmateCallback(object sender, EventArgs e)
@@ -115,21 +134,43 @@ namespace Assets.Game.Control
 
         private void CheckWinLoss()
         {
+            // Yay, full roster
             if (roster.IsFull())
             {
                 WinGame();
             }
 
-            // TODO - Not sure about this
-            //if (roster.IsEmpty())
-            //{
-            //    LoseGame();
-            //} 
+            // Too many parasites
+            if (stats.NumHunts >= 5)
+            {
+                LoseGame();
+            }
+
+            // Too many deaths
+            if (stats.NumDeaths >= 5)
+            {
+                LoseGame();
+            }
         }
 
         private void LoseGame()
         {
-            throw new NotImplementedException();
+            ui.ClearPotentialCrewmateDetails();
+            ui.ClearLog();
+
+            ui.AppendToLog("You have been fired as the head of HR");
+            ui.AppendToLog($"You let {stats.NumHunts} parasites onto the ship, killing {stats.NumDeaths} people!");
+            ui.AppendToLog("Game Over");
+
+            // Disable all buttons
+            DisableButtons();
+        }
+
+        private void DisableButtons()
+        {
+            ui.crewmateAccept -= AcceptCrewmateCallback;
+            ui.crewmateRefuse -= RefuseCrewmateCallback;
+            ui.crewmateNew -= NewCrewmateCallback;
         }
 
         private void WinGame()
@@ -142,9 +183,7 @@ namespace Assets.Game.Control
             ui.AppendToLog("Game Over");
 
             // Disable all buttons
-            ui.crewmateAccept -= AcceptCrewmateCallback;
-            ui.crewmateRefuse -= RefuseCrewmateCallback;
-            ui.crewmateNew -= NewCrewmateCallback;
+            DisableButtons();
         }
 
         private void AddCrewmate(HumanCrewmate crewmate)
@@ -152,29 +191,47 @@ namespace Assets.Game.Control
             // Add to roster
             roster.Add(crewmate);
 
-            // Move to roster
-            crewmate.transform.parent = roster.transform;
+            MoveToRoster(crewmate);
 
+            // Record stats
             stats.IncrementNumInRoster();
             ui.SetNumInRoster(stats.NumInRoster);
         }
 
-        private void BeginHunt(IHunter hunter)
+        private void MoveToRoster(HumanCrewmate crewmate)
+        {
+            // Move to roster
+            crewmate.transform.SetParent(roster.transform);
+
+            // Destination position
+            Vector3 destination = roster.transform.position + new Vector3(stats.NumInRoster, 0, 0);
+            crewmate.SetDestination(destination, 1);
+        }
+
+        private void BeginHunt(ParasiteCrewmate hunter)
         {
             // Start the hunt
-            ISet<IPrey> allPrey = hunter.GoHunting();
+            ISet<HumanCrewmate> allPrey = hunter.GoHunting();
 
             // Kill all the prey
-            foreach(IPrey p in allPrey)
+            int i = 0;
+            foreach (HumanCrewmate p in allPrey)
             {
-                roster.Remove((HumanCrewmate)p);  // This cast is gross
+                // No longer in the roster
+                roster.Remove(p);
+
+                // Dies
                 p.Die();
+
+                MoveToHunt(p, i);
+
+                i++;
             }
 
             ui.AppendToLog($"The following crewmates were killed: {string.Join(", ", allPrey)}");
 
             // Parasite escapes
-            hunter.Escape();
+            //hunter.Escape();
             ui.AppendToLog($"{potentialCrewmate} has escaped into space after his killing spree");
 
             // Update stats and UI
@@ -182,6 +239,16 @@ namespace Assets.Game.Control
             stats.AddNumDeaths(allPrey.Count);
             stats.SubtractNumInRoster(allPrey.Count);
             UpdateUI();
+        }
+
+        private void MoveToHunt(HumanCrewmate p, int index)
+        {
+            // Move to hunt
+            p.transform.SetParent(hunt.transform);
+
+            // Destination position
+            Vector3 destination = hunt.transform.position + new Vector3(stats.NumDeaths + index, 0, 0);
+            p.SetDestination(destination, 1);
         }
     }
 }
